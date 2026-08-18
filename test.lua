e14f31702eec6a86db755a475677f5322b762a94
scriptjame <132070412+scriptjame@users.noreply.github.com>
2026-08-19 02:38:31 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -129,6 +129,18 @@ local NoKeyScripts = {
     },
 
     {
+        Name = "Combat Hub",
+        Icon = "◇",
+        URL = "https://raw.githubusercontent.com/joshhhie/rise/refs/heads/main/loader.lua"
+    },
+
+    {
+        Name = "Argon Hub X",
+        Icon = "✦",
+        URL = "https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua"
+    },
+
+    {
         Name = "Allusive",
         Icon = "✧",
         Premium = true
